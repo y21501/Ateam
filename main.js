fd276c11d662e1a35d09b4240ba81e8c155f20ba
Atsushil1 <y21508@iwate-it.ac.jp>
2021-12-22 14:14:39 +0900

    主人公を追加しました

--- main.js
+++ main.js
@@ -8,65 +8,31 @@
     
 // });
 
-
-var canvas = document.getElementById("screan");//canvasを読み込む
+var canvas = document.getElementById("screeen");//canvasを読み込む
 var ctx = canvas.getContext("2d");
 var px = 40    //player x座標
 var py = 300    //player y座標
-
-var p_dx = 0    //player xの速さ
-var p_dy = 0    //player yの速さ
-
-//レーザーの座標
-var lx =[0];   //レーザーをたくさん描けるようにするためリストにする
-var ly = [0];
-
-//enemyの座標
-var ex = [0];
-var ey = [0];
+var p_dx = 0    //player xの速さ  //追加
+var p_dy = 0    //player yの速さ  //追加
 
 //playerを描く関数
 function player_draw(){
     ctx.beginPath()
-    ctx.rect(px,py,ps,ps)
+    ctx.rect(px,py,30,30)
     ctx.fillStyle = "#00ff00"
     ctx.fill()
     ctx.closePath()
 }
-
-function l_draw(){
-    for(var i = 0;i < lx.length;i++){
-        ctx.beginPath()
-        ctx.rect(lx[i],ly[i],15,6)
-        ctx.fillStyle = "#ff0000"
-        ctx.fill()
-        ctx.closePath()
-    }
-}
-function e_draw(){
-    for(var i = 0;i < lx.length;i++){
-        ctx.beginPath()
-        ctx.rect(ex[i],ey[i],36,36)
-        ctx.fillStyle="#ff00ff"
-        ctx.fill()
-        ctx.closePath()
-    }
-}
 //キーが押されたときに実行される
 document.onkeydown = function(e){
-    if(e.key == "ArrowUp"){  //↑
-        p_dx = 0
-        p_dy = -3
+    if(e.key == "ArrowLeft"){  //↑
+        p_dx = -3
+        p_dy = 0
     }
-    if(e.key == "ArrowDown"){//↓
-        p_dx = 0
-        p_dy = 3
+    if(e.key == "ArrowRight"){//↓
+        p_dx = 3
+        p_dy = 0
     }
-    if(e.key == " "){
-        lx.push(px)    //レーザー発射開始位置(playerの位置)をリストに追加
-        ly.push(py)
-    }
-    
 }
 //キーが離されたときに実行される
 document.onkeyup = function(e){
@@ -75,21 +41,97 @@ document.onkeyup = function(e){
 }
 
 function draw(){
-    ctx.clearRect(0/*開始地点*/,0,canvas.width/*終了地点*/,canvas.height)   //canvasをいったんクリアする
-    player_draw()
-    l_draw()
-    e_draw()
-    for(var i = 0;i < lx.length/*リストの長さ*/;i++){   //リストを読み込む
-        lx[i] += 2  //今読み込んでいるレーザーを動かす
-    }
-    e_draw()
-    for(var i = 0;i < ex.length/*リストの長さ*/;i++){   //リストを読み込む
-        ex[i] -= 2  //今読み込んでいるenemyを動かす
-    }
-    //playerを動かす
-    px += p_dx
-    py += p_dy
+    ctx.clearRect(0/*開始地点*/,0,canvas.width/*終了地点*/,canvas.height)   //canvasをいったんクリアする //追加
+    player_draw();
+    px += p_dx  //追加
+    py += p_dy  //追加
 }
-setInterval(draw,10)    //10ミリ秒単位で実行
-
+setInterval(draw,10);    //10ミリ秒単位で実行 //追加
 draw();
+
+
+
+// var canvas = document.getElementById("screeen");//canvasを読み込む
+// var ctx = canvas.getContext("2d");
+// var px =  200   //player x座標
+// var py = 700    //player y座標
+
+// var p_dx = 0    //player xの速さ
+// var p_dy = 0    //player yの速さ
+
+// //レーザーの座標
+// var lx =[0];   //レーザーをたくさん描けるようにするためリストにする
+// var ly = [0];
+
+// //enemyの座標
+// var ex = [0];
+// var ey = [0];
+
+// //playerを描く関数
+// function player_draw(){
+//     ctx.beginPath()
+//     ctx.rect(px,py,ps,ps)
+//     ctx.fillStyle = "#00ff00"
+//     ctx.fill()
+//     ctx.closePath()
+// }
+
+// function l_draw(){
+//     for(var i = 0;i < lx.length;i++){
+//         ctx.beginPath()
+//         ctx.rect(lx[i],ly[i],15,6)
+//         ctx.fillStyle = "#ff0000"
+//         ctx.fill()
+//         ctx.closePath()
+//     }
+// }
+// function e_draw(){
+//     for(var i = 0;i < lx.length;i++){
+//         ctx.beginPath()
+//         ctx.rect(ex[i],ey[i],36,36)
+//         ctx.fillStyle="#ff00ff"
+//         ctx.fill()
+//         ctx.closePath()
+//     }
+// }
+// //キーが押されたときに実行される
+// document.onkeydown = function(e){
+//     if(e.key == "ArrowUp"){  //↑
+//         p_dx = 0
+//         p_dy = -3
+//     }
+//     if(e.key == "ArrowDown"){//↓
+//         p_dx = 0
+//         p_dy = 3
+//     }
+//     if(e.key == " "){
+//         lx.push(px)    //レーザー発射開始位置(playerの位置)をリストに追加
+//         ly.push(py)
+//     }
+    
+// }
+// //キーが離されたときに実行される
+// document.onkeyup = function(e){
+//     p_dx = 0    //止める
+//     p_dy = 0
+// }
+
+// function draw(){
+//     ctx.clearRect(0/*開始地点*/,0,canvas.width/*終了地点*/,canvas.height)   //canvasをいったんクリアする
+//     player_draw()
+//     l_draw()
+//     e_draw()
+//     for(var i = 0;i < lx.length/*リストの長さ*/;i++){   //リストを読み込む
+//         lx[i] += 2  //今読み込んでいるレーザーを動かす
+//     }
+//     e_draw()
+//     for(var i = 0;i < ex.length/*リストの長さ*/;i++){   //リストを読み込む
+//         ex[i] -= 2  //今読み込んでいるenemyを動かす
+//     }
+//     //playerを動かす
+//     px += p_dx
+//     py += p_dy
+// }
+// setInterval(draw,10)    //10ミリ秒単位で実行
+
+// draw();
